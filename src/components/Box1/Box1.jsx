24776629e33e70817c93a14e91efbf4d1ca4b444
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-02-07 19:04:48 +0530

    Completely animated 1st box

--- src/components/Box1/Box1.jsx
+++ src/components/Box1/Box1.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from "framer-motion"
 
 export default function Box1() {
+
+    const [isAnimating, setIsAnimating] = useState(false)
+
     return (
         <div className="box-container">
-            <div className="box">
-                <></>
-            </div>
+            <motion.div
+                className="box"
+                animate={{
+                    x: isAnimating ? 800 : 0,
+                    opacity: isAnimating ? 1 : 0.2,
+                    rotate: isAnimating ? 360 : 0
+                }}
+                initial={{
+                    opacity: 0.2
+                }}
+                transition={{
+                    // duration: 2
+                    type: "spring",
+                    stiffness: 60,
+                    // damping: 20
+
+                }}
+
+                onClick={() => setIsAnimating(!isAnimating)}
+            >
+
+            </motion.div>
 
         </div>
     )
