f59c7fd4c824cdd51634b7149a87440a7351a40a
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-02-07 17:37:04 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import Box1 from "./components/Box1/Box1";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Box1 />
+    </div>
+  );
 }
 
 export default App;
